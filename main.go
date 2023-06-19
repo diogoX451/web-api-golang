82ae5c07743e6114a5b25d607bc59a2d39e741cc
Diogo Almeida <diogo.g07@icloud.com>
2023-06-19 13:26:47 -0300

    atualizacao

--- main.go
+++ main.go
@@ -3,31 +3,44 @@ package main
 import (
 	"net/http"
 
+	"os"
+
+	"github.com/joho/godotenv"
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Student struct {
-	ID       string `json:"id"`
-	NAME     string `json:"NAME"`
-	LASTNAME string `json:"LASTNAME"`
+	ID   string `json:"id"`
+	NAME string `json:"name"`
 }
 
-var student = []Student{
-	{ID: "1", NAME: "Diogo", LASTNAME: "Almeida"},
-	{ID: "2", NAME: "Guiller", LASTNAME: "jug"},
-	{ID: "3", NAME: "NAME3", LASTNAME: "LASTNAME3"},
-	{ID: "4", NAME: "NAME4", LASTNAME: "LASTNAME4"},
-	{ID: "5", NAME: "NAME5", LASTNAME: "LASTNAME5"},
-	{ID: "6", NAME: "NAME6", LASTNAME: "LASTNAME6"},
-	{ID: "7", NAME: "NAME7", LASTNAME: "LASTNAME7"},
-	{ID: "8", NAME: "NAME8", LASTNAME: "LASTNAME8"},
-	{ID: "9", NAME: "NAME9", LASTNAME: "LASTNAME9"},
-	{ID: "10", NAME: "NAME10", LASTNAME: "LASTNAME10"},
+var student []Student
+
+var err error
+var db *gorm.DB
+
+func init() {
+
+	if err := godotenv.Load(); err != nil {
+		panic("No .env variable")
+	}
+
+	db, err = gorm.Open(postgres.Open(os.Getenv("DB_DNS")), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func main() {
+
+	db.Raw("SELECT * FROM student").Scan(&student)
 	router := gin.Default()
-	router.GET("/", getStudent)
+	router.GET("/student", getStudent)
 	router.Run("localhost:3000")
 }
 
